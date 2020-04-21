effa3e40adfb907afa203b7f1a99b64a5f7792d0
Leon Si <leonzalion@gmail.com>
2020-04-21 18:15:27 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import Canvas from './canvas';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h3 style={{ textAlign: 'center' }}>Dos Paint</h3>
+        <div className="main">
+          <div className="color-guide">
+            <h5>Color Guide</h5>
+            <div className="user user">User</div>
+            <div className="user guest">Guest</div>
+          </div>
+          <Canvas />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
