b55ec6579e59b34631d48b76cadb0fa588807d29
Leon Si <leonzalion@gmail.com>
2020-04-22 11:18:19 -0400

    updates

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
+io.on('connection', (socket) => {
+  console.log('socket connected');
+  socket.on('paint', function(data) {
+    console.log(data);
+    socket.broadcast.emit('draw', data);
+  });
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
