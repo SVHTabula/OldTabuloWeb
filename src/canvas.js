28132d72213c98dd7881515de0f1e2e1fd9e619f
AbstractUltra <dragonhe36@gmail.com>
2020-04-22 03:41:39 +0100

    colors

--- src/canvas.js
+++ src/canvas.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { v4 } from "uuid";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
-const userStrokeStyle = "#EE92C2";
 const guestStrokeStyle = "#F0C987";
 const line = [];
 const userId = v4();
@@ -10,7 +9,8 @@ const socket = io(process.env.SERVER_URL);
 
 export default function DrawingCanvas() {
   const [isPainting, setIsPainting] = useState(false);
-  const [prevPos, setPrevPos] = useState({ offsetX: 0, offsetY: 0});
+  const [userStrokeStyle, setUserStrokeStyle] = useState("#EE92C2");
+  const [prevPos, setPrevPos] = useState({ offsetX: 0, offsetY: 0 });
   const canvas = useRef(null);
 
   useEffect(() => {
@@ -53,7 +53,7 @@ export default function DrawingCanvas() {
   function endPaintEvent() {
     if (isPainting) {
       setIsPainting(false);
-      socket.emit('paint', { line, userId });
+      socket.emit("paint", { line, userId });
       line.splice(0, line.length);
     }
   }
@@ -61,7 +61,7 @@ export default function DrawingCanvas() {
   function paint(prevPos, currPos, strokeStyle) {
     const { offsetX, offsetY } = currPos;
     const { offsetX: x, offsetY: y } = prevPos;
-    const ctx = canvas.current.getContext('2d');
+    const ctx = canvas.current.getContext("2d");
     ctx.beginPath();
     ctx.strokeStyle = strokeStyle;
     ctx.moveTo(x, y);
@@ -70,30 +70,37 @@ export default function DrawingCanvas() {
     setPrevPos({ offsetX, offsetY });
   }
 
+  function handleColorChange(event) {
+    setUserStrokeStyle(event.target.value)
+  }
+
   return (
     <div>
       <button
         onClick={() => {
-          canvas.current.getContext('2d').lineWidth -= 10;
+          canvas.current.getContext("2d").lineWidth -= 10;
         }}
       >
         -
       </button>
       <button
         onClick={() => {
-          canvas.current.getContext('2d').lineWidth += 10;
+          canvas.current.getContext("2d").lineWidth += 10;
         }}
       >
         +
       </button>
-      <canvas
-        ref={canvas}
-        style={{ background: "black" }}
-        onMouseDown={onMouseDown}
-        onMouseLeave={endPaintEvent}
-        onMouseUp={endPaintEvent}
-        onMouseMove={onMouseMove}
-      />
+      <textarea onChange={handleColorChange}></textarea>
+      <div>
+        <canvas
+          ref={canvas}
+          style={{ background: "black" }}
+          onMouseDown={onMouseDown}
+          onMouseLeave={endPaintEvent}
+          onMouseUp={endPaintEvent}
+          onMouseMove={onMouseMove}
+        />
+      </div>
     </div>
   );
 }
